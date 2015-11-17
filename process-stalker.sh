2830130083a0bf8f90e902e7ce917e27f2f8ab6a
Gavin Staniforth <gavin@usemarkup.com>
2015-11-17 17:46:15 +0000

    Added logging

--- process-stalker.sh
+++ process-stalker.sh
@@ -5,14 +5,20 @@ if [ ! -f /etc/process-stalker.conf ]; then
     exit 1
 fi
 
+touch "/var/log/process-stalker.log"
 input="/etc/process-stalker.conf"
+
+if [ ! -f "/var/log/process-stalker.log" ]; then
+    echo "No log file created"
+    exit 1
+fi
 
 while true
 do
     while IFS= read -r process
     do
       if [ `ps aux | grep -v ${process} | grep ${process} | wc -l` -eq 0 ]; then
-           echo "${process} is not currently running" `date +"%r %a %d %h %y"`
+           echo "${process} is not currently running" `date +"%r %a %d %h %y"` > "/var/log/process-stalker.log"
            service ${process} start
       fi
     done < "$input"
